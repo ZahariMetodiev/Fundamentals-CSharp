f17c836c5d2336ff08d016eb6390e5ecbc4219a8
ZahariMetodiev <zmetodiev4@gmail.com>
2022-12-15 12:35:42 +0200

    Add task MonthPrinter

---
+++ E01.Basic Syntax, Conditional Statements and Loops/T05.MonthPrinter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T05.MonthPrinter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] months = new string[]
+            {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
+
+            if (n <= 0 || n >= 13)
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+            Console.WriteLine(months[n - 1]);
+        }
+    }
+}
