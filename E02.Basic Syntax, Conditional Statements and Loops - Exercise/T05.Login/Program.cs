9bec446bdc1486d37a68f0dc83be744022d3e09f
ZahariMetodiev <zmetodiev4@gmail.com>
2022-12-16 10:38:06 +0200

    Add task Login

---
+++ E02.Basic Syntax, Conditional Statements and Loops - Exercise/T05.Login/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace T05.Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+            
+            int counter = 0;
+            string input = Console.ReadLine();
+
+            while (input != password)
+            {
+                Console.WriteLine("Incorrect password. Try again.");
+                counter++;
+                if (counter == 3)
+                {
+                    Console.WriteLine($"User {userName} blocked!");
+                    return;
+                }
+
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
