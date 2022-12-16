91960a4842513f8f2990c7d1b4f7e25c8bcccabd
ZahariMetodiev <zmetodiev4@gmail.com>
2022-12-16 10:36:27 +0200

    Add task Ages

---
+++ E02.Basic Syntax, Conditional Statements and Loops - Exercise/T01. Ages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T01._Ages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            string person = string.Empty;
+
+            if (age >= 0 && age <= 2)
+            {
+                person = "baby";
+            }
+            else if (age <= 13)
+            {
+                person = "child";
+            }
+            else if (age <= 19)
+            {
+                person = "teenager";
+            }
+            else if (age <= 65)
+            {
+                person = "adult";
+            }
+            else
+            {
+                person = "elder";
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
