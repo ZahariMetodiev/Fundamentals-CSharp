b5dded69381e9f5238f1fe4a95c4c9ca9c4bab70
ZahariMetodiev <zmetodiev4@gmail.com>
2023-01-10 11:21:15 +0200

    Add task SortNumbers

---
+++ E03.Basic Syntax, Conditional Statements and Loops - More Exercise/T01.SortNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Sort_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers[i] = number;
+            }
+
+            Array.Sort(numbers);
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
