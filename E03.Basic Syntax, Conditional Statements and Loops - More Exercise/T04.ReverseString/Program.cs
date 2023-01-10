feda8c0c9d6e9558eaa12772ea8215d77b5076e6
ZahariMetodiev <zmetodiev4@gmail.com>
2023-01-10 11:26:50 +0200

    Add task ReverseString

---
+++ E03.Basic Syntax, Conditional Statements and Loops - More Exercise/T04.ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T04.ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            char[] charArray = text.ToCharArray();
+            string reverse = string.Empty;
+
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                reverse += charArray[i];
+            }
+
+            Console.WriteLine(reverse);
+        }
+    }
+}
