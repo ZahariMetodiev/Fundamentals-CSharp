fb875db39e42fd1b14a2320a5eb897af1a507962
ZahariMetodiev <zmetodiev4@gmail.com>
2023-01-11 11:57:04 +0200

    Add task ConvertMetersТoKilometers

---
+++ E04.Data Types and Variables/T01.ConvertMetersТoKilometers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Т01.ConvertMetersТoKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double kilometers = meters / 1000;
+
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
